71bce888b1d2f9034fcf4d4af9c3e63b2c2f58dc
Faiyaz Rahman <faiyazrahman03@gmail.com>
2020-04-09 12:00:40 +0100

    Created register method

--- classes/User.php
+++ classes/User.php
@@ -1,6 +1,7 @@
 <?php
 
 class User{
+
     private $db;
 
     function __construct($connect)
@@ -8,5 +9,25 @@ class User{
         $this->db = $connect;
     }
 
+    //Register Method
+    public function register($uname, $uemail,$upass)
+    {
+        try
+        {
+             password_hash($upass, PASSWORD_DEFAULT);
+
+            $stmt = $this->db->prepare("INSERT INTO `users`(user_name,user_email,user_pass) VALUES (:uname, :uemail, :upass)");
+            $stmt->bindParam(":uname", $uname);
+            $stmt->bindParam(":uemail", $uemail);
+            $stmt->bindParam(":upass", $upass);
+            $stmt->execute();
+
+            return $stmt;
+
+        }catch(PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+    }
 
 }
